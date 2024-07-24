88bb9d5d3e1e0d01334c5f17ad8a690e2e4cc81d
Ben Strobel <benedikt.strobel@tum.de>
2024-07-25 01:04:00 +0200

    Set up city generator

--- Assets/Editor/EmercastGenerateBuildings.cs
+++ Assets/Editor/EmercastGenerateBuildings.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
+using Unity.Scenes;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Editor
 {
@@ -8,20 +12,26 @@ namespace Editor
         [MenuItem(("Emercast/Generate Buildings"))]
         public static void GenerateBuildings()
         {
+            var targetSceneObject = GameObject.FindGameObjectWithTag("GeneratorTargetScene");
+            var targetScene = targetSceneObject.GetComponent<SubScene>();
+            
             // Would be better to stream but works with big file, so good enough
-            var buildingsCSV = Resources.Load<TextAsset>("exported_buildings_50").text;
-            var lines = buildingsCSV.Split("\r\n");
-            Debug.Log(lines.Length);
-            foreach (var line in lines)
+            var gmlResource = Resources.Load<TextAsset>("gml-data/688_5334");
+            var gmlFile = gmlResource.text;
+            var lines = gmlFile.Split("\r\n");
+            Debug.Log("Lines in GML file: " + lines.Length);
+            var city = ParserGml.LoadGml(lines);
+            Debug.Log("Parsed " + city.Count + " buildings");
+            var i = 0;
+            foreach (var b in city)
             {
-                var segments = line.Split("|");
-                if(segments.Length != 4) continue;
-                var id = segments[0];
-                var height = segments[1];
-                var stories = segments[2];
-                var shape = segments[3];
-                Debug.Log(id);
+                b.Draw(targetScene.EditingScene);
+                Debug.Log("Created Building #" + i);
+                i++;
             }
+            
+            EditorSceneManager.MarkSceneDirty(targetScene.EditingScene);
+            EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
         }
     }
 }
---
+++ Assets/Editor/GmlConverter/ParserGml.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ParserGml
+{
+    static float[] StringArrayToFloatArray(string[] sa)
+    {
+        float[] res;
+
+        // Remove last element if null
+        if(sa[sa.Length-1] == "")
+            res = new float[sa.Length-1];
+        else
+            res = new float[sa.Length];
+
+        for (int i = 0; i < sa.Length;i++)
+        {
+            try
+            {
+                res[i] = float.Parse(sa[i].Replace('.', ','));
+            }
+            catch
+            {
+
+            }
+        }
+
+        return res;
+    }
+
+
+    public static List<Building> LoadGml(string[] lines)
+    {
+        List<Building> city = new List<Building>();
+        Building buildingtmp = new Building();
+
+        foreach (string l in lines )
+        {
+            // When reaching end of building tag, Add the current building to the city
+            if (l.Contains("</bldg:Building"))
+            {                
+                city.Add(buildingtmp);
+                buildingtmp = new Building();
+            }
+
+            if ( l.Contains("<gml:posList"))
+            {
+                string tmp = l.Substring(l.IndexOf(">")+1);
+                tmp = tmp.Substring(0, tmp.Length - "</gml:posList>".Length);
+                string[] valueString = tmp.Split(' ');
+                float[] valueFloat = StringArrayToFloatArray(valueString);
+                buildingtmp.AddPoly(valueFloat);               
+            }
+        }
+        return city;
+    }
+}
+
