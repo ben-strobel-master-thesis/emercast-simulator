f5c2ffdd71077e8172e59653ae88472eec047b12
Ben Strobel <benedikt.strobel@tum.de>
2024-07-24 02:10:55 +0200

    Set up unity project & committed exported building files

---
+++ Assets/Editor/EmercastGenerateBuildings.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+
+namespace Editor
+{
+    public class EmercastGenerateBuildings : UnityEditor.Editor
+    {
+        [MenuItem(("Emercast/Generate Buildings"))]
+        public static void GenerateBuildings()
+        {
+            
+        }
+    }
+}
