427957d1861415ca4ad1fbddd36b9a1ac2cfe8ee
Ben Strobel <benedikt.strobel@tum.de>
2024-07-24 02:32:58 +0200

    Loading buildings file

--- Assets/Editor/EmercastGenerateBuildings.cs
+++ Assets/Editor/EmercastGenerateBuildings.cs
@@ -1,4 +1,5 @@
 using UnityEditor;
+using UnityEngine;
 
 namespace Editor
 {
@@ -7,7 +8,20 @@ namespace Editor
         [MenuItem(("Emercast/Generate Buildings"))]
         public static void GenerateBuildings()
         {
-            
+            // Would be better to stream but works with big file, so good enough
+            var buildingsCSV = Resources.Load<TextAsset>("exported_buildings_50").text;
+            var lines = buildingsCSV.Split("\r\n");
+            Debug.Log(lines.Length);
+            foreach (var line in lines)
+            {
+                var segments = line.Split("|");
+                if(segments.Length != 4) continue;
+                var id = segments[0];
+                var height = segments[1];
+                var stories = segments[2];
+                var shape = segments[3];
+                Debug.Log(id);
+            }
         }
     }
 }
